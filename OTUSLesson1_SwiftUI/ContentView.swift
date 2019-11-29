a930e490c392bc0653b73431721fce6c041bd402
Scherbinin Andrey <ascherbinin@live.ru>
2019-11-29 07:58:05 +0500

    Initial Commit

---
+++ OTUSLesson1_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OTUSLesson1_SwiftUI
+//
+//  Created by Scherbinin Andrey on 29.11.2019.
+//  Copyright © 2019 Scherbinin Andrey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
